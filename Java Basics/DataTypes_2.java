502e80ded89e2a685eee1871a4536a41d22434d8
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2023-12-25 14:14:32 +0530

    Create DataTypes_2.java

---
+++ Java Basics/DataTypes_2.java
@@ -0,0 +1,9 @@
+// Two data types
+
+// Primitive Data type
+// int, float, double, String, char, boolean, byte, short
+
+// Non Primitive Data type
+// They are called reference type
+// strings, arrays, and classes
+
