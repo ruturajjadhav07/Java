211b8418b0299d39c839c2cc020a7da6d128c5b2
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-01-01 16:13:20 +0530

    Update SingleLevel.java

--- Java Core/Inheritance/SingleLevel.java
+++ Java Core/Inheritance/SingleLevel.java
@@ -5,8 +5,7 @@
 // The class created from base class is called derived class or subclass.It is done by extend keyword.
 // By inheriting classes we can reuse code.
 // It allows to perform multiple operations.
-// Types are single level,multi level,hierarchical
-
+// Types are single level,multilevel,hierarchical,multiple
 
 //Remember if we create class inside main file (class file name) first we need to create instance of main class file and then next of our created class.
 
