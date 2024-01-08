87f2635daacfaf6509c1c3ec289e3a1f537fa2b9
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-01-08 21:27:53 +0530

    Update Final.java

--- Java Core/Final Keyword/Final.java
+++ Java Core/Final Keyword/Final.java
@@ -17,12 +17,11 @@ final class A {
 
 }
 
-// class B extends A{ // It will Give error The type B cannot subclass the final
-// class A.It will stop Inheritance
+// class B extends A{ // It will Give error The type B cannot subclass the final class A.It will stop Inheritance
 //
 // }
 
-public class Final {
+public class Final {       // method name is final.It is not declared as final(Keep in mind).
 
     static class C {
         static final void show() {
@@ -37,30 +36,36 @@ public class Final {
 
     static class D extends C {
         static void show1() { // After making static make show1.It cannot use same method name because it is made final and stops method overriding.
+           
             // If not made final use show()
-            System.out.println("Sairaj");
+            System.out.println("Sairaj");   // Trying to override a final method will result in an error.
         }
     }
 
     public static void main(String[] args) {
 
         System.out.println("\n==================================");
-        // Using Final variavble
+        
+        // Using Final variable
         final int n = 9;
         System.out.println("Final variable value is  " + n);
-        // n = 10; // This line will give error as you cant cange the value of n it is
-        // final variable
+        
+        // n = 10; // This line will give error as you cant change the value of n it is declared final variable.
 
         System.out.println("\n==================================");
 
         D obj = new D();
+        
         // It will override to Sairaj.
 
-        obj.show(); // if we use final in Class C it will stop overriding and prints Ruturaj.
+        obj.show(); //we use final in Class C it will stop overriding (Sairaj) and prints Ruturaj.
         obj.method1(3, 9);
 
         // To better understand first run with static and then just remove static from class C and run the code and check output and make show() in class D.
-
+        // That is  Modify the code by removing final keywords and observe changes to better understand their impact.
     }
 
 }
+
+
+
