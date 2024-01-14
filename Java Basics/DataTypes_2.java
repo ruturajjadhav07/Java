a1b1bb3f8dcb4210b0559868116b768734835e00
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-01-14 16:44:22 +0530

    Update DataTypes_2.java

--- Java Basics/DataTypes_2.java
+++ Java Basics/DataTypes_2.java
@@ -7,3 +7,6 @@
 // They are called reference type
 // strings, arrays, and classes
 
+
+// Basically Primitive Data Types are the basic building blocks, such as int (for integers), float, double, String, char, boolean, byte, and short.
+// and Non-Primitive Data Types are also called reference types, these include more complex structures like strings, arrays, and classes.
