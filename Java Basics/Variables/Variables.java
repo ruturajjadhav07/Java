d16fdcff8c7cffacd1a28862aa5d9ac9864de894
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-02-01 16:56:09 +0530

    Create Variables.java

---
+++ Java Basics/Variables/Variables.java
@@ -0,0 +1,28 @@
+// Variables are used to store a specific value
+// There are different types of variables
+// String,int,float,double,boolean and char.
+// These are also called data types
+
+
+public class Variables_1 {
+
+    public static void main(String[] args) {
+
+        // If we write anything after data type it is called variable.
+
+        String name = "Hello";  // String can be letters and numbers if it is printed in double quotes " ".
+        int num = 22;           // int is used to declare number.
+        float num1 = 22;        // double is used to declare number value in points.It is used whenever you need a number with a decimal.
+        double num2 = 33.20;    // same like float.
+        char name1 = 'W';       //char is data type that stores single value or character.It is always surrounded by single quotes ' '.
+        boolean bool = true;    // boolean is data type which have two values true and false.
+
+
+        //    Printing the values
+        System.out.println(name); // We are calling variable names that is name which are declared.
+        System.out.println(num);
+        System.out.println(num1); //It will print 22.0
+        System.out.println(num2);
+        System.out.println(bool);
+    }
+}
