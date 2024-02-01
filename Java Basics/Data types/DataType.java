41ae5ce302c5edf4af06d386ba35431f7ae5127a
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-02-01 16:56:58 +0530

    Create DataType.java

---
+++ Java Basics/Data types/DataType.java
@@ -0,0 +1,18 @@
+// Two data types
+
+// Primitive Data type
+
+// Integer  -> byte, short, int, long
+// Float -> double, float
+// Character -> char
+// Boolean -> true or false
+
+
+
+// Non Primitive Data type
+// They are called reference type
+// strings, arrays, and classes
+
+
+// Basically Primitive Data Types are the basic building blocks, such as int (for integers), float, double, String, char, boolean, byte, and short.
+// and Non-Primitive Data Types are also called reference types, these include more complex structures like strings, arrays, and classes.
