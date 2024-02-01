b5012327bcf0ba351b6647b3a17768b00dcfc0a4
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-02-01 16:59:48 +0530

    Create Comment.java

---
+++ Java Basics/Comment/Comment.java
@@ -0,0 +1,8 @@
+public class Comment {
+
+
+    public static void main(String[] args) {
+        System.out.println("This is comment = //");    //they are used to highlight the code and what its function is.
+    }
+    //This is comment
+}
