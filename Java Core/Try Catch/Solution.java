13390e16303ad7892ca7aa2fd23e234cc98ceee7
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-02-06 19:20:21 +0530

    Update Solution.java

--- Java Core/Try Catch/Solution.java
+++ Java Core/Try Catch/Solution.java
@@ -1,1 +1,20 @@
+public class Solution {
 
+    public static void main(String[] args) {
+        int n = 10;
+        try {
+            if (n / 0 == 0) { // Just consider this.
+                System.out.println("Even Number..!"); // We cant divide number by zero it will throw error and catch the
+                                                      // error.
+            } else {
+                System.out.println("Odd Number..!");
+            }
+        } catch (Exception e) { // e is refered to Exception class
+
+            // System.out.println(e.getMessage()); // This will print "/ by zero" i.e. can't
+            // divide by zero.
+            System.out.println(e); // As e is referred to exception class it will catch error and
+                                   // print as java.lang.ArithmeticException: / by zero
+        }
+    }
+}
