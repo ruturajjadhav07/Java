3b3a3cb90e33279a61e9d13fac8d7eb6b76f01ff
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-02-16 21:49:15 +0530

    Update Solution.java

--- Java Core/Exception Handling/Finally block/Solution.java
+++ Java Core/Exception Handling/Finally block/Solution.java
@@ -1,1 +1,18 @@
+public class Solution {
 
+    public static void main(String[] args) {
+
+        int i = 10;
+        int j = 0;
+
+        // also try with j = 2
+        try {
+            int result = i / j;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("I can't divide by zero");
+        } finally {   // finally block
+            System.out.println("This block always executes, regardless of whether an exception occurred or not.");
+        }
+    }
+}
