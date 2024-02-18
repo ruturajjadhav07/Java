0bda6138504141ebbd9251a208964fe7cbf7a728
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-02-18 17:22:23 +0530

    Update Solution.java

--- Java Core/Java Collection Framework/Set /Hashset/Solution.java
+++ Java Core/Java Collection Framework/Set /Hashset/Solution.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class Solution1 {
+public class Solution {
 
     public static void main(String[] args) {
       // Create a new Hashset called nums that holds integers
